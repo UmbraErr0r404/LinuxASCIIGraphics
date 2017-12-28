3a6a4546e56cbd77b1268f42b52b0a5d0305a73d
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2017-12-27 22:35:20 -0600

    Made ncurses display a string.

--- TesterMain.cpp
+++ TesterMain.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include "ScreenManager.hpp"
+#include <ncurses.h> //base graphics
+#include <chrono> //time 
+#include <thread> //sleep
 
 int main(int argc, char **argv) {
+	
+	using namespace std::chrono_literals;
 
-  std::cout << "Hello, World!" << std::endl;
+	ScreenManager Boss("Boss");
 
-  return 0;
+	std::cout << "Hello, World!" << std::endl;
+
+	initscr();
+
+	//std::stdscr.T
+	int h, w;
+
+	getmaxyx(stdscr, h, w);
+
+	WINDOW * win = newwin(h, w, 0, 0);
+	
+	wmove(win, 2, 2);
+
+	waddstr(win, "Hello World!");
+	
+	waddch(win,'?');
+
+	wrefresh(win);
+
+	std::this_thread::sleep_for(10s);	
+
+	return 0;
 }
