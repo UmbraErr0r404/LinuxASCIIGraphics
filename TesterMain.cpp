e8b780a95b5ef36a17d9571554a781117ccf762c
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2017-12-29 23:52:52 -0600

    Fixed a reordering error

--- TesterMain.cpp
+++ TesterMain.cpp
@@ -4,20 +4,25 @@
 #include <ncurses.h> //base graphics
 #include <thread>    //sleep
 
+// http://www.asciitable.com/
+
 using namespace std::chrono_literals;
 
-int main(int argc, char **argv) {
+int main(/*int argc, char **argv*/) {
 
   int h, w;
-  WINDOW *win = newwin(h, w, 0, 0);
 
   initscr();
   getmaxyx(stdscr, h, w);
+
+  WINDOW *win = newwin(h, w, 0, 0);
+
   wmove(win, 2, 2);
   waddstr(win, "Hello World!");
   waddch(win, '?');
   wrefresh(win);
   std::this_thread::sleep_for(10s);
+  endwin();
 
   return 0;
 }
