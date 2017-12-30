541ec6412c814cdbd291b1f585483961dffe0061
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2017-12-29 23:28:06 -0600

    Deleted old tests to pervent lava code

--- TesterMain.cpp
+++ TesterMain.cpp
@@ -1,35 +1,23 @@
-#include <iostream>
 #include "ScreenManager.hpp"
+#include <chrono> //time
+#include <iostream>
 #include <ncurses.h> //base graphics
-#include <chrono> //time 
-#include <thread> //sleep
+#include <thread>    //sleep
+
+using namespace std::chrono_literals;
 
 int main(int argc, char **argv) {
-	
-	using namespace std::chrono_literals;
 
-	ScreenManager Boss("Boss");
+  int h, w;
+  WINDOW *win = newwin(h, w, 0, 0);
 
-	std::cout << "Hello, World!" << std::endl;
+  initscr();
+  getmaxyx(stdscr, h, w);
+  wmove(win, 2, 2);
+  waddstr(win, "Hello World!");
+  waddch(win, '?');
+  wrefresh(win);
+  std::this_thread::sleep_for(10s);
 
-	initscr();
-
-	//std::stdscr.T
-	int h, w;
-
-	getmaxyx(stdscr, h, w);
-
-	WINDOW * win = newwin(h, w, 0, 0);
-	
-	wmove(win, 2, 2);
-
-	waddstr(win, "Hello World!");
-	
-	waddch(win,'?');
-
-	wrefresh(win);
-
-	std::this_thread::sleep_for(10s);	
-
-	return 0;
+  return 0;
 }
